c28254679cdafeb56744e3545a5dc7ed8037d590
Forever Inc dev <132741319+hadrian-grey@users.noreply.github.com>
2023-05-22 19:22:11 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(".owl-carousel").owlCarousel({
+	loop: true,
+    margin: 10,
+	nav: false,
+	autoplay: true, 
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true ,
+    responsiveClass: true,
+    loop: true,
+	responsive: {
+		0: {
+			items: 1,
+			
+		},
+		600: {
+			items: 1,
+			
+		},
+		1000: {
+			items: 1,
+		},
+	},
+});
